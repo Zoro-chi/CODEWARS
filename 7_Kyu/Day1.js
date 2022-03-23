a3b1460221bacbd7902e1347b4e9e89ebcf73b9c
zoro <jagbetuyi001@gmail.com>
2022-03-23 19:26:00 +0100

    first kata submission

---
+++ 7_Kyu/Day1.js
@@ -0,0 +1,24 @@
+// KATA FOR WEDNESDAY, 23/3/2022
+
+
+// Description:
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+
+
+// My Solution:
+
+function sortByLength (array) {
+  return array.sort((a, b) => a.length - b.length);
+};
