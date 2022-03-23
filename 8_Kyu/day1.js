fd6d178c7cee02b57e5ed84a0e03fb2fac18d86f
zoro <jagbetuyi001@gmail.com>
2022-03-23 19:54:39 +0100

    23/3/22

---
+++ 8_Kyu/day1.js
@@ -0,0 +1,20 @@
+// KATA FOR WEDNESDAY, 23/3/2022
+
+
+// Description:
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+// My Solution:
+
+function noSpace(x){
+  return x.split(" ").join("")
+}
+
+// My Notes on Kata :
+
+// This was a lot easier to work through than the 7 kyu I did earlier 
+// I feel to be more efficient on learning i should continue the fundamentals till i feel more comfortable.
+// This took me 6 mins to pass. More work to be done.
+
